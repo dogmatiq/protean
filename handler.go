13cf0d57a3fd8c6aecaa79f21323c1200b400023
James Harris <mailjamesharris@gmail.com>
2021-09-22 16:51:21 +1000

    Move `handler.servePOST()` into its own file.

--- handler.go
+++ handler.go
@@ -2,17 +2,14 @@ package protean
 
 import (
 	"fmt"
-	"io"
 	"net/http"
 	"strconv"
 	"strings"
 
 	"github.com/dogmatiq/protean/internal/proteanpb"
 	"github.com/dogmatiq/protean/internal/protomime"
-	"github.com/dogmatiq/protean/middleware"
 	"github.com/dogmatiq/protean/rpcerror"
 	"github.com/dogmatiq/protean/runtime"
-	"google.golang.org/protobuf/proto"
 )
 
 // Handler is an http.Handler that maps HTTP requests to RPC calls.
@@ -116,159 +113,6 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	h.servePOST(w, r, method)
-}
-
-// servePOST serves an RPC request made using the HTTP Post method.
-func (h *handler) servePOST(
-	w http.ResponseWriter,
-	r *http.Request,
-	method runtime.Method,
-) {
-	unmarshaler, inputMediaType, ok, err := unmarshalerByNegotiation(r)
-	if err != nil {
-		httpError(
-			w,
-			http.StatusBadRequest,
-			protomime.TextMediaTypes[0],
-			protomime.TextMarshaler,
-			rpcerror.New(
-				rpcerror.Unknown,
-				"the Content-Type header is missing or invalid",
-			),
-		)
-		return
-	}
-	if !ok {
-		httpError(
-			w,
-			http.StatusUnsupportedMediaType,
-			protomime.TextMediaTypes[0],
-			protomime.TextMarshaler,
-			rpcerror.New(
-				rpcerror.Unknown,
-				"the server does not support the '%s' media-type supplied by the client",
-				inputMediaType,
-			).WithDetails(
-				&proteanpb.SupportedMediaTypes{
-					MediaTypes: protomime.MediaTypes,
-				},
-			),
-		)
-		return
-	}
-
-	marshaler, outputMediaType, ok, err := marshalerByNegotiation(r)
-	if err != nil {
-		httpError(
-			w,
-			http.StatusBadRequest,
-			protomime.TextMediaTypes[0],
-			protomime.TextMarshaler,
-			rpcerror.New(
-				rpcerror.Unknown,
-				"the Accept header is invalid",
-			),
-		)
-		return
-	}
-	if !ok {
-		httpError(
-			w,
-			http.StatusNotAcceptable,
-			protomime.TextMediaTypes[0],
-			protomime.TextMarshaler,
-			rpcerror.New(
-				rpcerror.Unknown,
-				"the client does not accept any of the media-types supported by the server",
-			).WithDetails(
-				&proteanpb.SupportedMediaTypes{
-					MediaTypes: protomime.MediaTypes,
-				},
-			),
-		)
-		return
-	}
-
-	data, err := io.ReadAll(r.Body)
-	if err != nil {
-		httpError(
-			w,
-			http.StatusInternalServerError,
-			outputMediaType,
-			marshaler,
-			rpcerror.New(
-				rpcerror.Unknown,
-				"the request body could not be read",
-			),
-		)
-		return
-	}
-
-	call := method.NewCall(r.Context(), middleware.Validator{})
-	defer call.Done()
-
-	// Send never blocks on unary RPC methods.
-	if _, err := call.Send(func(in proto.Message) error {
-		return unmarshaler.Unmarshal(data, in)
-	}); err != nil {
-		httpError(
-			w,
-			http.StatusBadRequest,
-			outputMediaType,
-			marshaler,
-			rpcerror.New(
-				rpcerror.Unknown,
-				"the RPC input message could not be unmarshaled from the request body",
-			),
-		)
-		return
-	}
-
-	out, _, err := call.Recv()
-	if err != nil {
-		if err, ok := err.(rpcerror.Error); ok {
-			httpError(
-				w,
-				httpStatusFromErrorCode(err.Code()),
-				outputMediaType,
-				marshaler,
-				err,
-			)
-		} else {
-			httpError(
-				w,
-				http.StatusInternalServerError,
-				outputMediaType,
-				marshaler,
-				rpcerror.New(
-					rpcerror.Unknown,
-					"the RPC method returned an unrecognized error",
-				),
-			)
-		}
-
-		return
-	}
-
-	data, err = marshaler.Marshal(out)
-	if err != nil {
-		httpError(
-			w,
-			http.StatusInternalServerError,
-			outputMediaType,
-			marshaler,
-			rpcerror.New(
-				rpcerror.Unknown,
-				"the RPC output message could not be marshaled to the response body",
-			),
-		)
-		return
-	}
-
-	w.Header().Add("Content-Type", protomime.FormatMediaType(outputMediaType, out))
-	w.Header().Add("Content-Length", strconv.Itoa(len(data)))
-	w.WriteHeader(http.StatusOK)
-	_, _ = w.Write(data)
 }
 
 // resolveMethod looks up the RPC method based on the request URL.
