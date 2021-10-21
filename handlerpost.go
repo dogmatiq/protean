c80f6976b61c88b046459bdef865a22d8194860c
James Harris <mailjamesharris@gmail.com>
2021-10-21 15:11:06 +1000

    Move content negotiation into `handlerpost.go`.

--- handlerpost.go
+++ handlerpost.go
@@ -1,14 +1,18 @@
 package protean
 
 import (
+	"errors"
 	"io"
+	"mime"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/dogmatiq/protean/internal/proteanpb"
 	"github.com/dogmatiq/protean/internal/protomime"
 	"github.com/dogmatiq/protean/rpcerror"
 	"github.com/dogmatiq/protean/runtime"
+	"github.com/elnormous/contenttype"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -23,68 +27,8 @@ func (h *handler) servePOST(
 		return
 	}
 
-	unmarshaler, inputMediaType, ok, err := unmarshalerByNegotiation(r)
-	if err != nil {
-		httpError(
-			w,
-			http.StatusBadRequest,
-			protomime.TextMediaTypes[0],
-			protomime.TextMarshaler,
-			rpcerror.New(
-				rpcerror.Unknown,
-				"the Content-Type header is missing or invalid",
-			),
-		)
-		return
-	}
+	marshaler, unmarshaler, outputMediaType, ok := negotiateMediaTypes(w, r)
 	if !ok {
-		httpError(
-			w,
-			http.StatusUnsupportedMediaType,
-			protomime.TextMediaTypes[0],
-			protomime.TextMarshaler,
-			rpcerror.New(
-				rpcerror.Unknown,
-				"the server does not support the '%s' media-type supplied by the client",
-				inputMediaType,
-			).WithDetails(
-				&proteanpb.SupportedMediaTypes{
-					MediaTypes: protomime.MediaTypes,
-				},
-			),
-		)
-		return
-	}
-
-	marshaler, outputMediaType, ok, err := marshalerByNegotiation(r)
-	if err != nil {
-		httpError(
-			w,
-			http.StatusBadRequest,
-			protomime.TextMediaTypes[0],
-			protomime.TextMarshaler,
-			rpcerror.New(
-				rpcerror.Unknown,
-				"the Accept header is invalid",
-			),
-		)
-		return
-	}
-	if !ok {
-		httpError(
-			w,
-			http.StatusNotAcceptable,
-			protomime.TextMediaTypes[0],
-			protomime.TextMarshaler,
-			rpcerror.New(
-				rpcerror.Unknown,
-				"the client does not accept any of the media-types supported by the server",
-			).WithDetails(
-				&proteanpb.SupportedMediaTypes{
-					MediaTypes: protomime.MediaTypes,
-				},
-			),
-		)
 		return
 	}
 
@@ -266,4 +210,154 @@ func (h *handler) parseContentLength(
 	}
 
 	return contentLength, true
+}
+
+// negotiateMediaTypes negotiates the media types used to marshal and unmarshal
+// RPC input/output messages.
+//
+// ok is false if the media-types can not be negotiated, in which case an error
+// response has already been written to w.
+func negotiateMediaTypes(
+	w http.ResponseWriter,
+	r *http.Request,
+) (
+	marshaler protomime.Marshaler,
+	unmarshaler protomime.Unmarshaler,
+	outputMediaType string,
+	ok bool,
+) {
+	unmarshaler, inputMediaType, ok, err := unmarshalerByNegotiation(r)
+	if err != nil {
+		httpError(
+			w,
+			http.StatusBadRequest,
+			protomime.TextMediaTypes[0],
+			protomime.TextMarshaler,
+			rpcerror.New(
+				rpcerror.Unknown,
+				"the Content-Type header is missing or invalid",
+			),
+		)
+		return nil, nil, "", false
+	}
+
+	if !ok {
+		httpError(
+			w,
+			http.StatusUnsupportedMediaType,
+			protomime.TextMediaTypes[0],
+			protomime.TextMarshaler,
+			rpcerror.New(
+				rpcerror.Unknown,
+				"the server does not support the '%s' media-type supplied by the client",
+				inputMediaType,
+			).WithDetails(
+				&proteanpb.SupportedMediaTypes{
+					MediaTypes: protomime.MediaTypes,
+				},
+			),
+		)
+		return nil, nil, "", false
+	}
+
+	marshaler, outputMediaType, ok, err = marshalerByNegotiation(r)
+	if err != nil {
+		httpError(
+			w,
+			http.StatusBadRequest,
+			protomime.TextMediaTypes[0],
+			protomime.TextMarshaler,
+			rpcerror.New(
+				rpcerror.Unknown,
+				"the Accept header is invalid",
+			),
+		)
+		return nil, nil, "", false
+	}
+
+	if !ok {
+		httpError(
+			w,
+			http.StatusNotAcceptable,
+			protomime.TextMediaTypes[0],
+			protomime.TextMarshaler,
+			rpcerror.New(
+				rpcerror.Unknown,
+				"the client does not accept any of the media-types supported by the server",
+			).WithDetails(
+				&proteanpb.SupportedMediaTypes{
+					MediaTypes: protomime.MediaTypes,
+				},
+			),
+		)
+
+		return nil, nil, "", false
+	}
+
+	return marshaler, unmarshaler, outputMediaType, true
+}
+
+// unmarshalerByNegotiation returns the unmarshaler to use for unmarshaling the
+// given request based on its Content-Type header.
+//
+// If the media types is not supported, ok is false.
+func unmarshalerByNegotiation(r *http.Request) (u protomime.Unmarshaler, mediaType string, ok bool, err error) {
+	mediaType = r.Header.Get("Content-Type")
+	if mediaType == "" {
+		return nil, "", false, errors.New("content type is empty")
+	}
+
+	// Parse media type both to validate and to strip the parameters.
+	mediaType, _, err = mime.ParseMediaType(mediaType)
+	if err != nil {
+		return nil, "", false, err
+	}
+
+	u, ok = protomime.UnmarshalerForMediaType(mediaType)
+
+	return u, mediaType, ok, nil
+}
+
+// marshalerByNegotiation returns the marshaler to use for marshaling
+// responses to the given request based on its Accept headers.
+//
+// If none of the supported media types are accepted, ok is false.
+func marshalerByNegotiation(r *http.Request) (m protomime.Marshaler, mediaType string, ok bool, err error) {
+	if len(r.Header.Values("Accept")) == 0 {
+		// If no Accept header is provided, respond using the same content type
+		// that the client supplied for the RPC input method.
+		mediaType = r.Header.Get("Content-Type")
+	} else {
+		t, _, err := contenttype.GetAcceptableMediaType(r, protoAcceptMediaTypes)
+		if err != nil && err != contenttype.ErrNoAcceptableTypeFound {
+			return nil, "", false, err
+		}
+
+		mediaType = t.String()
+	}
+
+	m, ok = protomime.MarshalerForMediaType(mediaType)
+
+	return m, mediaType, ok, nil
+}
+
+// protoAcceptMediaTypes is the set of media types that can be used for
+// marshaling protobuf messages, in the format consumed by the
+// github.com/elnormous/contenttype package.
+var protoAcceptMediaTypes []contenttype.MediaType
+
+// acceptPostHeader is the value to use for the Accept-Post header in HTTP
+// responses.
+var acceptPostHeader = strings.Join(
+	protomime.MediaTypes,
+	", ",
+)
+
+func init() {
+	for _, mediaType := range protomime.MediaTypes {
+		protoAcceptMediaTypes = append(
+			protoAcceptMediaTypes,
+			contenttype.NewMediaType(mediaType),
+		)
+	}
 }
